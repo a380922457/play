2ace52e3c289bc0718506e652931d17a72e8c5c6
a380922457 <380922457@qq.com>
2017-10-27 11:38:27 +0800

    validation

---
+++ image_caption/coco_api.py
@@ -0,0 +1,31 @@
+import json
+import hashlib
+import sys
+import os
+
+filename = '/media/father/d/ai_challenger_caption_validation_20170910/val.json'
+output_name = '/media/father/d/ai_challenger_caption_validation_20170910/test100.json'
+
+with open(filename, "r") as f:
+    data = json.load(f)
+
+new_data = {}
+annotations = []
+images = []
+for i, line in enumerate(data):
+    if i > 100: break
+    image_id = line["image_id"]
+    captions = line["caption"]
+    hash_id = int(int(hashlib.sha256(image_id).hexdigest(), 16) % sys.maxint)
+    for k, caption in enumerate(captions):
+        annotation = {"id": i * 5 + k + 1, "caption": caption, "image_id": hash_id}
+        annotations.append(annotation)
+
+        image = {"file_name": os.path.splitext(image_id)[0], "id": hash_id}
+        images.append(image)
+
+new_data["annotations"] = annotations
+new_data["images"] = images
+
+with open(output_name, "w") as f:
+    json.dump(new_data, f)
